5aa112a46a7e3e455b5ec18fee2d46089ab3a176
jesus <jelopezgonzalez@gmail.com>
2022-07-17 20:12:47 +0100

    add components header, navigation, inicio, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
